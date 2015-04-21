aab25b52ca57e19324f4de188ac191bafe60d324
Wei-ju Wu <weiju.wu@gmail.com>
2015-04-21 15:14:02 -0700

    added a simple Shock client that supports authentication, upload and download

---
+++ lib/shock.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import requests
+
+
+class ShockClient:
+    """A convenience class to access a Shock service
+    It handles the aspects:
+
+    - Authentication
+    - Upload
+    - Download
+    """
+    def __init__(self, base_url, auth_token):
+        self.base_url = base_url
+        self.auth_token = auth_token
+
+    def auth_headers(self):
+        return {'Authorization': 'OAuth %s' % self.auth_token}
+
+    def upload_file(self, path):
+        files = {'upload': open(path)}
+        r = requests.post(self.base_url + '/node', files=files,
+                          headers=self.auth_headers())
+        return r.json()
+
+    def node_info(self, node_id):
+        r = requests.get(self.base_url + '/node/%s' % node_id,
+                         headers=self.auth_headers())
+        return r.json()
+
+    def download_file(self, node_id, target_path):
+        r = requests.get(self.base_url + '/node/%s?download_raw' % node_id,
+                         headers=self.auth_headers(), stream=True)
+        with open(target_path, 'wb') as outfile:
+            for chunk in r.iter_content(256):
+                outfile.write(chunk)
